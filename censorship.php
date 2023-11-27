3dee5427f0184a20a157ccd052b3058f641e05b2
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-27 15:59:30 +0100

    printed original and censored paragraph

--- censorship.php
+++ censorship.php
@@ -6,6 +6,21 @@
   <title>PHP Badwords</title>
 </head>
 <body>
+
+  <?php
+
+  $paragraph = $_GET["paragraph"];
+  $censoredWord = $_GET["censored_word"];
+
+  echo "Il tuo paragrafo originale : $paragraph <br>";
+  echo "La lunghezza del paragrafo è : " . strlen($paragraph) . " caratteri <br>";
+
+  $censoredParagraph = str_replace($censoredWord, '***', $paragraph);
+
+  echo "Questo invece è il tuo paragrafo censurato : $censoredParagraph <br>";
+  echo "La lunghezza del paragrafo censurato è : " . strlen($censoredParagraph) . " caratteri";
+
+  ?>
   
 </body>
 </html>
