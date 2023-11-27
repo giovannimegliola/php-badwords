193570223056719d64177de7e819bec82e43fa4c
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-27 15:35:04 +0100

    added form content

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
   <title>PHP Badwords</title>
 </head>
 <body>
-  
+  <form action="censorship.php" method="get">
+    
+    <p>Scrivi qui il tuo paragrafo:</p> 
+    <textarea name="paragraph" id="paragraph" cols="30" rows="10"></textarea>
+    
+    <p>Quale parola vuoi censurare?</p> 
+    <input type="text" name="censored_word" id="censored_word">
+
+    <input type="submit" value="Invia">
+
+  </form>
 </body>
 </html>
